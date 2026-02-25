5e27e79703c7ba2404e3b7b52f64c6c805d98ba3
reddsf <bourosalexandru7@gmail.com>
2026-02-25 02:19:35 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyC1phDEwFAqMKdBk5R7sVb0yf87UOgIyis",
+  authDomain: "rrepohub.firebaseapp.com",
+  projectId: "rrepohub",
+  storageBucket: "rrepohub.appspot.com",
+  messagingSenderId: "337942603496",
+  appId: "1:337942603496:web:d37b2ba55d37c95cc30b5d"
+};
